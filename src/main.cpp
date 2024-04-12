3320fb883435b826cfa80d71b5ff5438b3d97357
David Mason <davmason@microsoft.com>
2024-04-12 15:58:13 -0700

    WIP

---
+++ src/main.cpp
@@ -0,0 +1,52 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+#include <eventheader/TraceLoggingProvider.h>
+
+#include <inttypes.h>
+#include <limits.h>
+#include <stdio.h>
+#include <string.h>
+#include <linux/user_events.h>
+
+const char *data_file = "/sys/kernel/tracing/user_events_data";
+
+static int event_reg(int fd, const char *command, int *write, volatile int *enabled)
+{
+    struct user_reg reg = {0};
+
+    reg.size = sizeof(reg);
+    reg.enable_bit = 31;
+    reg.enable_size = sizeof(*enabled);
+    reg.enable_addr = (__u64)enabled;
+    reg.name_args = (__u64)command;
+
+    if (ioctl(fd, DIAG_IOCSREG, &reg) == -1)
+        return -1;
+
+    *write = reg.write_index;
+
+    return 0;
+}
+
+int main()
+{
+    int data_fd, simple_write, big_write;
+    __u32 count = 0;
+
+    data_fd = open(data_file, O_RDWR);
+
+    if (event_reg(data_fd, "test u32 iteration", &simple_write, &simple_enabled) == -1)
+    {
+        printf("error user_events: %s\n", strerror(errno));
+        return errno;
+    }
+
+    if (event_reg(data_fd, "test u32 iteration", &simple_write, &simple_enabled) == -1)
+    {
+        printf("error user_events: %s\n", strerror(errno));
+        return errno;
+    }
+    
+    return 0;
+}
